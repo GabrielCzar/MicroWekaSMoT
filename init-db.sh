e15754ed3b069a26bcfa40885278d26f25d697a8
Gabriel César <gcal1@live.com>
2018-10-02 16:18:28 -0300

    update init db

--- init-db.sh
+++ init-db.sh
@@ -1,1 +1,2 @@
-docker run --name "postgis" -e POSTGRES_USER="dbuser" -e POSTGRES_PASS="dbpass" -e POSTGRES_DBNAME="dbtest" -p 25432:5432 -d -t kartoza/postgis
+#!/usr/bin/env bash
+docker run --name "postgis" -e POSTGRES_USER="dbuser" -e POSTGRES_PASS="dbpass" -e POSTGRES_DBNAME="dbtest" -p 25432:5432 l --restart=always -d -t kartoza/postgis
